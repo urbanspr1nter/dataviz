515ae8850a0f1bbf717258950fe8c4d2cd53bd78
Roger Ngo <urbanspr1nter@gmail.com>
2020-07-23 08:06:26 -0700

    push

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https//urbanspr1nter.github.io/dataviz/precache-manifest.4291c9b9898788d23f6f189f56fa874d.js"
+  "/precache-manifest.1099c324cee23215c4d7286fe0d78027.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https//urbanspr1nter.github.io/dataviz/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
